234e84b6541bf2221e853d0fe5c44b154e07fabe
Irfan <irfanyusufk13@gmail.com>
2023-09-08 17:02:00 +0700

    CS01 - Menyelesaikan Cs modul 1

--- JBus/irfanYusufJBusRA/JBus.java
+++ JBus/irfanYusufJBusRA/JBus.java
@@ -8,20 +8,23 @@ public class JBus
         boolean isDiscount = true;
         int beforeDiscount= 1000;
         int afterDiscount= 900;
-        int price = 5000;
+        int price = 1000;
         float discountPercentage=100.0f;
         int discountedPrice= 900;
         int numberOfSeat=2;
+            
         
         
-        
-        system.out.println("" + getBusId);
-        system.out.println("" + getBusName);
-        system.out.println("" + isDiscount);
-        system.out.println("" + getDiscountPercentage);
-        system.out.println("" + getDiscountedPercentage);
-        system.out.println("" + getOriginalPrice);
-        system.out.println("" + getAdminFeePercentage);
+        System.out.println("Bus ID: " + getBusId);
+        System.out.println("Bus Name: " + getBusName);
+        System.out.println("" + isDiscount);
+        System.out.println("Harga: " + price);
+        System.out.println("Diskon: " + getDiscountPercentage (beforeDiscount,afterDiscount));
+        System.out.println("harga sesudah didiskon: " + getDiscountedPercentage (price,discountPercentage));
+        System.out.println("harga asli: " + getOriginalPrice(1000, 10.0f));
+        System.out.println("admin fee: " + getAdminFeePercentage () );
+        System.out.println("harga biaya admin: " + getAdminFee (price));
+        System.out.println("Total Harga: " + getTotalPrice (price,numberOfSeat) );
         
         
         
@@ -30,46 +33,62 @@ public class JBus
     }
     
 
-int getBusId(){
+public static int getBusId(){
     return 0;
 }
     
-String getBusName(){
+public static String getBusName(){
     return "Bus";
 }
     
-boolean isDiscount(){
+public static boolean isDiscount(){
     return true;   
 }
     
-float getDiscountPercentage(int beforeDiscount,int afterDiscount ){
-       if (beforeDiscount< afterDiscount);
+public static float getDiscountPercentage(int beforeDiscount,int afterDiscount ){
+       float discountPercentage=0.0f;
+       discountPercentage = (afterDiscount / beforeDiscount) * 1;
+    
+        if (beforeDiscount < afterDiscount){
        return 0.0f;
+    }
+    
+    return discountPercentage;
        
 }
 
-int getDiscountedPercentage(int price, float discountPercentage){
-    if (discountPercentage>100.0f);
-    return 0;
+public static int getDiscountedPercentage(int price, float discountPercentage){
+    if (discountPercentage > 100.0f){
+        return 0;
+    } 
+    int discountedPrice;
+    discountedPrice = price - (int) (price * (discountPercentage / 100));
+    return price;
 }
 
     
-int getOriginalPrice(int discountedPrice,float discountPercentage){
-    
+public static int getOriginalPrice(int discountedPrice,float discountPercentage){
+    float originalPrice= 0.0f;
+    originalPrice = discountedPrice * (100/ (100 - discountPercentage));
+    return (int)originalPrice;
 }
 
-float getAdminFeePercentage(){
+public static float getAdminFeePercentage(){
     return 0.05f;
 }
 
-int getAdminFee(int price){
-    int harga;
-    harga= price * 0.5;
+
+public static int getAdminFee(int price){
+    float adminFee= 0.0f;
+    adminFee= price * 0.05f;
+    return (int)adminFee;
 }
 
-int getTotalPrice(int price,  int numberOfSeat ){
-    if(price =5000 && numberofseat=2)
-        return 5250;
+public static int getTotalPrice(int price,  int numberOfSeat ){
+    float totalPrice = 0.0f;
+    totalPrice= (price * numberOfSeat) + price * 0.05f;
+    return (int)totalPrice;
+}
 }
 
     
